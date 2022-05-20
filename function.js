a45c4803d25c2503074d883cd8e465438135db85
padmasri-09 <102460374+padmasri-09@users.noreply.github.com>
2022-05-20 16:37:24 +0530

    Add files via upload

---
+++ function.js
@@ -0,0 +1,15 @@
+//1st
+function square(x) { return x * x; }
+function double(x) { return x * 2; }
+let composedValue(square,double,5);
+console.log(composedValue(square,double,5));
+
+//4th
+var number = [2,5,7,8,9];
+var newArray=[];
+var SquareAllArray=function(numbers){
+    for(var i=0; i<numbers.length; i++){
+        newArray.push(Math.pow(numbers[i],2));
+    }
+    console.log(newArray);
+}
